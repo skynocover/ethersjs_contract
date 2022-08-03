1e452a0cc5e23d8bbc7bd5c8dc1d71e5e1fd2f1a
EricWu <skynocover@gmail.com>
2022-08-03 19:06:08 +0800

    feat: compile and deploy

--- index.js
+++ index.js
@@ -3,11 +3,69 @@ document.addEventListener('alpine:init', () => {
     on: false,
   });
 
-  Alpine.data('dropdown', () => ({
-    open: false,
+  Alpine.data('contract', () => ({
+    address: '',
+    title: 'myTitle',
+    content: `
+    //SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
 
-    toggle() {
-      this.open = !this.open;
+contract Greeter {
+  string private greeting;
+
+  event TestEvent(address from, string str);
+
+  constructor(string memory _greeting) {
+    greeting = _greeting;
+  }
+
+  function greet() public view returns (string memory) {
+    return greeting;
+  }
+
+  function setGreeting(string memory _greeting) public returns (string memory) {
+    emit TestEvent(msg.sender, _greeting);
+    greeting = _greeting;
+    return greeting;
+  }
+}
+
+    `,
+    abi: [],
+    bytecode: '',
+
+    contractAddress: '',
+
+    async getAddress() {
+      const signer = await provider.getSigner();
+      this.address = await signer.getAddress();
+    },
+
+    async compile() {
+      const { data } = await axios.post(
+        // 'http://localhost:3000/api/contract/compile',
+        'https://contractcompile-neq2kr3vhq-de.a.run.app/api/contract/compile',
+        {
+          title: this.title,
+          content: this.content,
+        },
+      );
+
+      if (data.res == 'OK') {
+        this.abi = data.abi;
+        this.bytecode = data.bytecode;
+      }
+    },
+
+    async deploy() {
+      const signer = await provider.getSigner();
+      const factory = new ethers.ContractFactory(this.abi, this.bytecode, signer);
+
+      // input deploy args
+      const contract = await factory.deploy('Greeting!');
+
+      console.log(contract);
+      this.contractAddress = contract.address;
     },
   }));
 
